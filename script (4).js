4e7b36974852462c47d32eaeb283f04d9e1b3342
Zaki31 <66411402+rahmadmudzaki31@users.noreply.github.com>
2022-07-12 14:35:15 +0700

    Add files via upload

---
+++ script (4).js
@@ -0,0 +1,3 @@
+var data = new array[1,1,1,2,2,4,1,1,2,1,2,2,2,8,1,1,3,3,3,3,2,4,1,1];
+var Max = math.max.apply(null,nilai);
+                document.getElementById("hasil").innerHTML = 'total data paling banyak keluar dari bilangan [1,1,1,2,2,4,1,1,2,1,2,2,2,8,1,1,3,3,3,3,2,4,1,1] adalah '+max;
